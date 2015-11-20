d383994cf1d52f73b942a1cc592bb18f7af30dcd
toretate <mahomahomahomaho.tt@gmail.com>
2015-11-21 03:41:19 +0900

    Tab分けし、アンナとハッシュタグを表示するように修正

--- app/src/main/java/com/toretate/aigistwclient/MainActivity.java
+++ app/src/main/java/com/toretate/aigistwclient/MainActivity.java
@@ -1,80 +1,66 @@
 package com.toretate.aigistwclient;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
-import io.fabric.sdk.android.Fabric;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.*;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.models.Tweet;
-import com.twitter.sdk.android.tweetui.*;
 
 import io.fabric.sdk.android.Fabric;
+import layout.TabFragment;
 
 
-public class MainActivity extends AppCompatActivity {
-
-	// Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-	private static final String TWITTER_KEY = "66gsxBA0dccvtroeHnmUJNfhQ";
-	private static final String TWITTER_SECRET = "8tbn8YFHabqNJuQ0QTBetKgmfaJJfGuljIuubq3slRLCej1YYh";
-
+public class MainActivity extends AppCompatActivity implements TabFragment.OnFragmentInteractionListener, ViewPager.OnPageChangeListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+		TwitterDefInterface twitterDef = new TwitterDefImpl();
+		TwitterAuthConfig authConfig = twitterDef.createTwitterAuthConfig();
 		Fabric.with(this, new Twitter(authConfig));
 
 		setContentView(R.layout.activity_main);
 
+		TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+		ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+
+		FragmentPagerAdapter adapter = new FragmentPagerAdapter( getSupportFragmentManager() ) {
+			@Override
+			public Fragment getItem(int position) {
+				return TabFragment.newInstance( position +1 );
+			}
+
+			@Override
+			public CharSequence getPageTitle(int position) {
+				if( position == 0 ) {
+					return "政務官アンナ";
+				} else {
+					return "#千年戦争アイギス";
+				}
+			}
+
+			@Override
+			public int getCount() {
+				return 2;
+			}
+		};
+
+		viewPager.setAdapter( adapter );
+		viewPager.addOnPageChangeListener(this);
+		tabLayout.setupWithViewPager( viewPager );
+
 		// TODO: Use a more specific parent
 		final ViewGroup parentView = (ViewGroup) getWindow().getDecorView().getRootView();
-
-		UserTimeline userTimeline = new UserTimeline.Builder()
-				.includeReplies(false)
-				.includeRetweets(false)
-				.maxItemsPerRequest(5)
-				.screenName("Aigis1000")
-				.build();
-		final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
-				.setTimeline(userTimeline)
-				.build();
-
-		ListView twitter = (ListView)this.findViewById( R.id.twitter );
-		twitter.setAdapter( adapter );
-
-		final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout)this.findViewById( R.id.swipeRefresh );
-		swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-			@Override
-			public void onRefresh() {
-				swipeRefreshLayout.setRefreshing( true );
-				adapter.refresh(new Callback<TimelineResult<Tweet>>() {
-					@Override
-					public void success(Result<TimelineResult<Tweet>> result) {
-						swipeRefreshLayout.setRefreshing( false );
-					}
-
-					@Override
-					public void failure(TwitterException e) {
-						// 失敗通知(Toastとかで？)
-					}
-				});
-			}
-		});
 
 //		// TODO: Base this Tweet ID on some data from elsewhere in your app
 //		long tweetId = 631879971628183552L;
@@ -91,18 +77,17 @@ public class MainActivity extends AppCompatActivity {
 //			}
 //		});
 
-
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
+//		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//		fab.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View view) {
+//				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//						.setAction("Action", null).show();
+//			}
+//		});
 	}
 
 	@Override
@@ -125,5 +110,25 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onFragmentInteraction(Uri uri) {
+
+	}
+
+	@Override
+	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+	}
+
+	@Override
+	public void onPageSelected(int position) {
+
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int state) {
+
 	}
 }
