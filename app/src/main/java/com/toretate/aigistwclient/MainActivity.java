ae524c2d665ee17bdd8219c4ea69b0a615aefcd0
toretate <toretatenee@gmail.com>
2015-12-04 00:47:34 +0900

    タップ時にTwitterに飛ぶのではなく、その場で TweetView で表示するように修正

--- app/src/main/java/com/toretate/aigistwclient/MainActivity.java
+++ app/src/main/java/com/toretate/aigistwclient/MainActivity.java
@@ -42,16 +42,20 @@ public class MainActivity extends AppCompatActivity implements TabFragment.OnFra
 
 			@Override
 			public CharSequence getPageTitle(int position) {
-				if( position == 0 ) {
-					return "政務官アンナ";
-				} else {
+				switch( position ) {
+				case 0:
+					return "運営";
+				case 1:
 					return "#千年戦争アイギス";
+				case 2:
+				default:
+					return "作者";
 				}
 			}
 
 			@Override
 			public int getCount() {
-				return 2;
+				return 3;
 			}
 		};
 
@@ -61,21 +65,6 @@ public class MainActivity extends AppCompatActivity implements TabFragment.OnFra
 
 		// TODO: Use a more specific parent
 		final ViewGroup parentView = (ViewGroup) getWindow().getDecorView().getRootView();
-
-//		// TODO: Base this Tweet ID on some data from elsewhere in your app
-//		long tweetId = 631879971628183552L;
-//		TweetUtils.loadTweet(tweetId, new Callback<Tweet>() {
-//			@Override
-//			public void success(Result<Tweet> result) {
-//				TweetView tweetView = new TweetView(MainActivity.this, result.data);
-//				parentView.addView(tweetView);
-//			}
-//
-//			@Override
-//			public void failure(TwitterException exception) {
-//				Log.d("TwitterKit", "Load Tweet failure", exception);
-//			}
-//		});
 
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
