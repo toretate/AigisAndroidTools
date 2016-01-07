7aad44c56f7aa40d266d44d66ab2f96e41a92c87
toretate <toretatenee@gmail.com>
2016-01-07 10:05:23 +0900

    gradle設定が変になっていたので修正 aigis1000_A用の設定

---
+++ app/src/main/java/com/toretate/aigisandroidtools/TwitterDefImpl.java
@@ -0,0 +1,18 @@
+package com.toretate.aigisandroidtools;
+
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+/**
+ * Created by toretate on 2015/11/21.
+ */
+public class TwitterDefImpl implements TwitterDefInterface {
+	// Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+	private static final String TWITTER_KEY = "66gsxBA0dccvtroeHnmUJNfhQ";
+	private static final String TWITTER_SECRET = "8tbn8YFHabqNJuQ0QTBetKgmfaJJfGuljIuubq3slRLCej1YYh";
+
+	public TwitterAuthConfig createTwitterAuthConfig() {
+		TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+		return authConfig;
+	}
+
+}
