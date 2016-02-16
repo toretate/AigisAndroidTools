2b83fbbaa0de33e0fc70f803096cc9df95675fa9
toretate <mahomahomahomaho.tt@gmail.com>
2016-02-16 10:34:54 +0900

    Tweetの表示をCustomTweetViewに変更など

--- app/src/main/java/com/toretate/aigisandroidtools/MainActivity.java
+++ app/src/main/java/com/toretate/aigisandroidtools/MainActivity.java
@@ -3,8 +3,6 @@ package com.toretate.aigisandroidtools;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
