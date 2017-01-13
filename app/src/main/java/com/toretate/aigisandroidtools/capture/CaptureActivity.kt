603a3a61b890dc860946314a83183d419d7aeb4e
toretate <toretate@gmail.com>
2017-01-13 19:53:32 +0900

    Capture実験

---
+++ app/src/main/java/com/toretate/aigisandroidtools/capture/CaptureActivity.kt
@@ -0,0 +1,44 @@
+package com.toretate.aigisandroidtools.capture
+
+import android.annotation.TargetApi
+import android.app.Activity
+import android.app.Service
+import android.content.Intent
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+class CaptureActivity : Activity() {
+
+    companion object {
+        private const val REQUEST_CAPTURE = 1
+
+        var projection: MediaProjection? = null
+    }
+
+    private lateinit var mediaProjectionManager: MediaProjectionManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mediaProjectionManager = getSystemService(Service.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), REQUEST_CAPTURE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                projection = mediaProjectionManager.getMediaProjection(resultCode, data)
+                val intent = Intent(this, CaptureService::class.java).setAction(CaptureService.ACTION_ENABLE_CAPTURE)
+                startService(intent)
+            } else {
+                projection = null
+                Toast.makeText( this, "Capture Error!", Toast.LENGTH_SHORT ).show()
+//                Toast.makeText(this, R.string.capture_error, Toast.LENGTH_SHORT).show()
+            }
+        }
+        finish()
+    }
+}
