f0716c8cf94cc1e38669ecc68f69b31dd723faf8
toretate <toretate@gmail.com>
2017-01-23 20:14:42 +0900

    OCR範囲を選択するための矩形選択UIの、範囲選択の方向固定UIを実装

--- app/src/main/java/com/toretate/aigisandroidtools/capture/SelectRectView.java
+++ app/src/main/java/com/toretate/aigisandroidtools/capture/SelectRectView.java
@@ -22,6 +22,16 @@ public class SelectRectView extends View {
     private Rect m_rect = new Rect( 100, 100, 200, 200 );
     private Paint m_paint = null;
 
+    private boolean m_fixL = false, m_fixT = false, m_fixR = false, m_fixB = false;
+    public void setFixL( boolean fix ) { m_fixL = fix; }
+    public void setFixT( boolean fix ) { m_fixT = fix; }
+    public void setFixR( boolean fix ) { m_fixR = fix; }
+    public void setFixB( boolean fix ) { m_fixB = fix; }
+    public boolean getFixL() { return m_fixL; }
+    public boolean getFixT() { return m_fixT; }
+    public boolean getFixR() { return m_fixR; }
+    public boolean getFixB() { return m_fixB; }
+
     public void setSelectRect( Rect rect ) {m_rect = rect; }
     public Rect getSelectRect() { return m_rect; }
 
@@ -73,35 +83,56 @@ public class SelectRectView extends View {
             this.getParent().requestDisallowInterceptTouchEvent(true);
             int x = (int)Math.floor( event.getX() );
             int y = (int)Math.floor( event.getY() );
-            if( x < m_rect.left ) {
-                m_rect.left = x;
-            } else if( m_rect.right < x ) {
-                m_rect.right = x;
+
+            if( m_fixL && m_fixR ) {
+                // x方向固定
+            } else if( m_fixL == false && m_fixR == true ) {
+                // Lのみ開放
+                m_rect.left = Math.min( x, m_rect.right -1 );
+            } else if( m_fixL == true && m_fixR == false ) {
+                // Rのみ開放
+                m_rect.right = Math.max( m_rect.left +1, x );
             } else {
-                // l < x < r なので近い方を合わせる
-                int dl = x - m_rect.left;
-                int dr = m_rect.right - x;
-                if( dl < dr ) m_rect.left = x;
-                else m_rect.right = x;
+                // 両開放
+                if( x < m_rect.left ) {
+                    m_rect.left = x;
+                } else if( m_rect.right < x ) {
+                    m_rect.right = x;
+                } else {
+                    // l < x < r なので近い方を合わせる
+                    int dl = x - m_rect.left;
+                    int dr = m_rect.right - x;
+                    if( dl < dr ) m_rect.left = x;
+                    else m_rect.right = x;
+                }
             }
 
-            if( y < m_rect.top ) {
-                m_rect.top = y;
-            } else if( m_rect.bottom < y ) {
-                m_rect.bottom = y;
+            if( m_fixT && m_fixB ) {
+                // y方向固定
+            } else if( m_fixT == false && m_fixB == true ) {
+                // Tのみ開放
+                m_rect.top = Math.min( y, m_rect.bottom -1 );
+            } else if( m_fixT == true && m_fixB == false ) {
+                // Bのみ開放
+                m_rect.bottom = Math.max( m_rect.top +1, y );
             } else {
-                // t < y < b なので近い方を合わせる
-                int dt = y - m_rect.top;
-                int db = m_rect.bottom - y;
-                if( dt < db ) m_rect.top = y;
-                else m_rect.bottom = y;
+                if( y < m_rect.top ) {
+                    m_rect.top = y;
+                } else if( m_rect.bottom < y ) {
+                    m_rect.bottom = y;
+                } else {
+                    // t < y < b なので近い方を合わせる
+                    int dt = y - m_rect.top;
+                    int db = m_rect.bottom - y;
+                    if( dt < db ) m_rect.top = y;
+                    else m_rect.bottom = y;
+                }
             }
+
             this.invalidate();
             return true;
         } else {
-            int x = (int)Math.floor( event.getX() );
-            int y = (int)Math.floor( event.getY() );
-            return true;
+            return false;
         }
     }
 }
