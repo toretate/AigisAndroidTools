7917c507573a4ec8409c136d4d66bcb364d19776
toretate <toretate@gmail.com>
2017-01-24 16:15:36 +0900

    不必要メソッドの削除と、OCRボタンを押した後はAsyncTaskで処理を行うように変更

--- app/src/main/java/com/toretate/aigisandroidtools/MainNavDrawer.java
+++ app/src/main/java/com/toretate/aigisandroidtools/MainNavDrawer.java
@@ -41,10 +41,6 @@ import io.fabric.sdk.android.Fabric;
  */
 public class MainNavDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, ViewPager.OnPageChangeListener, ViewPagerContentFragment.OnFragmentInteractionListener {
 
-	public interface ScreenshotPermissionListener { void screenshotPermissionCompleted(); }
-	private static ScreenshotPermissionListener s_screenshotPermisionListener;
-	public static void setScreenshotPermissionListener( ScreenshotPermissionListener l ) { s_screenshotPermisionListener = l; }
-
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "66gsxBA0dccvtroeHnmUJNfhQ";
     private static final String TWITTER_SECRET = "8tbn8YFHabqNJuQ0QTBetKgmfaJJfGuljIuubq3slRLCej1YYh";
