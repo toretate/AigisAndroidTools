666bb742df26ab085a7e0f489197e201167b29fd
toretate <toretatenee@gmail.com>
2017-01-27 18:23:41 +0900

    とりあえずコミット

--- app/src/main/java/com/toretate/aigisandroidtools/capture/ChaStaInfoView.kt
+++ app/src/main/java/com/toretate/aigisandroidtools/capture/ChaStaInfoView.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.CompoundButton
 import android.widget.LinearLayout
 import android.widget.ToggleButton
 import com.toretate.aigisandroidtools.R
@@ -13,16 +14,23 @@ import com.toretate.aigisandroidtools.R
  * Created by toretate on 2017/01/24.
  */
 class ChaStaInfoView : LinearLayout {
-    constructor(context: Context?) : super(context) { init(context) }
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) { init(context) }
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) { init(context) }
 
-    private fun init( context : Context? ) {
+    private val m_root : View
+    private var m_areaSelectButton : ToggleButton? = null
+
+    constructor(context: Context?) : super(context) { m_root = init(context) }
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) { m_root = init(context) }
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) { m_root = init(context) }
+
+    private fun init( context : Context? ) : View {
         var root = View.inflate( context, R.layout.cha_sta_info_view, this )
 
-        var areaSelectButton = root.findViewById( R.id.areaselect_button ) is ToggleButton
+        m_areaSelectButton = root.findViewById( R.id.areaselect_button ) as ToggleButton
+        return root;
+    }
 
-
+    public fun setAreaSelectButtonChangeListener( l : CompoundButton.OnCheckedChangeListener ) {
+        m_areaSelectButton?.setOnCheckedChangeListener( l )
     }
 
 }
