84745b2cc0285720d8e29790b17040c9389d6a24
toretate <toretatenee@gmail.com>
2017-03-10 18:35:31 +0900

    OCR範囲を複数持てるように構造変更

--- app/src/main/java/com/toretate/aigisandroidtools/capture/ChaStaInfoView.kt
+++ app/src/main/java/com/toretate/aigisandroidtools/capture/ChaStaInfoView.kt
@@ -1,13 +1,12 @@
 package com.toretate.aigisandroidtools.capture
 
 import android.content.Context
+import android.provider.MediaStore
+import android.support.annotation.IdRes
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.CompoundButton
-import android.widget.LinearLayout
-import android.widget.ToggleButton
+import android.widget.*
 import com.toretate.aigisandroidtools.R
 
 /**
@@ -15,8 +14,15 @@ import com.toretate.aigisandroidtools.R
  */
 class ChaStaInfoView : LinearLayout {
 
+    public interface ChaStaInfoViewListener {
+        /** 「範囲選択」ボタンが変更された */
+        public fun OnAreaSelectButtonChanged();
+    }
+
     private val m_root : View
     private var m_areaSelectButton : ToggleButton? = null
+
+    public var listener : ChaStaInfoViewListener? = null;
 
     constructor(context: Context?) : super(context) { m_root = init(context) }
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) { m_root = init(context) }
@@ -25,12 +31,14 @@ class ChaStaInfoView : LinearLayout {
     private fun init( context : Context? ) : View {
         var root = View.inflate( context, R.layout.cha_sta_info_view, this )
 
-        m_areaSelectButton = root.findViewById( R.id.areaselect_button ) as ToggleButton
-        return root;
-    }
+        m_areaSelectButton = root.findViewById( R.id.areaselect_button ) as ToggleButton?
+        m_areaSelectButton?.setOnCheckedChangeListener( object:CompoundButton.OnCheckedChangeListener {
+            override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
+                listener?.OnAreaSelectButtonChanged()
+            }
+        } )
 
-    public fun setAreaSelectButtonChangeListener( l : CompoundButton.OnCheckedChangeListener ) {
-        m_areaSelectButton?.setOnCheckedChangeListener( l )
+        return root;
     }
 
 }
